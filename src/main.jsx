70131fb693787f66907d8f08561243a1c6266a3a
JulioSantana121314 <sv73015804@idat.pe>
2026-01-10 23:57:54 -0500

    buen import bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/App.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './styles/App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
